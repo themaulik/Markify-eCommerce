175e4dcd0114d69bafbfcfe84b797148f0fa1e36
Maulik Goswami <72465849+themaulik@users.noreply.github.com>
2022-08-08 22:32:06 +0530

    so many changes in the lots of files

---
+++ inc/footer.php
@@ -0,0 +1,147 @@
+    <!-- ! Footer Started -->
+    <!-- Footer -->
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-dark text-muted">
+      <!-- Section: Social media -->
+      <section
+        class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+      >
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+          <span>Get connected with us on social networks:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-google"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-linkedin"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+        <!-- Right -->
+      </section>
+      <!-- Section: Social media -->
+
+      <!-- Section: Links  -->
+      <section class="">
+        <div class="container text-center text-md-start mt-5">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <!-- Content -->
+              <h6 class="text-white text-uppercase fw-bold mb-4">
+                <i class="fas fa-gem me-3"></i>MARKIFY
+              </h6>
+              <p class="text-white">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Inventore, vel voluptatem recusandae quos accusamus pariatur
+                harum aliquid ipsum dicta, saepe possimus, laborum officia
+                fugiat sapiente.
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-white text-uppercase fw-bold mb-4">Products</h6>
+              <p class="text-white">
+                <a href="#!" class="text-reset" style="text-decoration: none;">Gen-z</a>
+              </p>
+              <p class="text-white">
+                <a href="#!" class="text-reset" style="text-decoration: none;">T-shirt</a>
+              </p>
+              <p class="text-white">
+                <a href="#!" class="text-reset" style="text-decoration: none;">Chinos</a>
+              </p>
+              <p class="text-white">
+                <a href="#!" class="text-reset" style="text-decoration: none;">Hoodies</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-white text-uppercase fw-bold mb-4">
+                Useful links
+              </h6>
+              <p class="text-white">
+                <a href="#!" class="text-reset" style="text-decoration: none;">Pricing</a>
+              </p>
+              <p class="text-white">
+                <a href="#!" class="text-reset" style="text-decoration: none;">Settings</a>
+              </p>
+              <p class="text-white">
+                <a href="#!" class="text-reset" style="text-decoration: none;">Orders</a>
+              </p>
+              <p class="text-white">
+                <a href="#!" class="text-reset" style="text-decoration: none;">Help</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <!-- Links -->
+              <h6 class="text-white text-uppercase fw-bold mb-4">Contact</h6>
+              <p class="text-white">
+                <i class="fas fa-home me-3"></i> New York, NY 10012, US
+              </p>
+              <p class="text-white">
+                <i class="fas fa-envelope me-3"></i>
+                info@markify.com
+              </p>
+              <p class="text-white">
+                <i class="fas fa-phone me-3"></i> + 01 234 567 88
+              </p>
+              <p class="text-white">
+                <i class="fas fa-print me-3"></i> + 01 234 567 89
+              </p>
+            </div>
+            <!-- Grid column -->
+          </div>
+          <!-- Grid row -->
+        </div>
+      </section>
+      <!-- Section: Links  -->
+
+      <!-- Copyright -->
+      <div
+        class="text-white text-center p-4"
+        style="background-color: rgba(0, 0, 0, 0.05)"
+      >
+        © 2021 Copyright:
+        <a class="text-white text-reset fw-bold" href="#">markify.com</a>
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+    <!-- ! Footer Bar Ends -->
+
+    <!-- ! Scripting Tag Started -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+      crossorigin="anonymous"
+    ></script>
+    <script src="/src/js/main.js"></script>
+    <!-- ! Scripting Tag Ends -->
+  </body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="/src/css/style.css" />
+  <title>Markify - Book For You</title>
+</head>
+
+<body>
+
+<nav class="navbar navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Markify</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Markify Menu</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Login & Regester</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</nav>
