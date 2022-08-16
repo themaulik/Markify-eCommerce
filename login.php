60463b9f79b7914f604c37a670ba6e19d8c6c33b
Maulik Goswami <72465849+themaulik@users.noreply.github.com>
2022-08-16 18:02:04 +0530

    same as before

--- login.php
+++ login.php
@@ -4,8 +4,8 @@
   <div class="row justify-content-center ">
     <div class="my-5 col-md-4 col-md-offset-4">
       <h1 class="text-uppercase">Login Form</h1>
-      <form action="./include/login.nic.php" method="POST">
-      <div class="form-floating mb-3 border border-dark rounded">
+      <form action="include/login.nic.php" method="POST">
+        <div class="form-floating mb-3 border border-dark rounded">
           <input type="text" name="name" class="form-control" id="floatingInput" placeholder="Username">
           <label for="floatingInput">Full Name</label>
         </div>
--- register.php
+++ register.php
@@ -4,7 +4,7 @@
   <div class="row justify-content-center">
     <div class="col-md-4 col-md-offset-4">
       <h1 class="text-uppercase">registration form</h1>
-      <form action="./include/register.nic.php" method="POST">
+      <form action="include/register.nic.php" method="POST">
         <div class="form-floating mb-3 border border-dark rounded">
           <input type="text" name="name" class="form-control" id="floatingInput" placeholder="Username">
           <label for="floatingInput">Full Name</label>
