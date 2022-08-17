2126461e2147c9c9fc338d3c0c58fd861323f9d5
Maulik Goswami <72465849+themaulik@users.noreply.github.com>
2022-08-17 10:04:23 +0530

    spacing the register.nic.php

--- include/register.nic.php
+++ include/register.nic.php
@@ -4,17 +4,16 @@ if (isset($_POST["submit"])) {
   $name = $_POST["name"];
   $uid = $_POST["email"];
   $email = $_POST["uid"];
-  $pwd =$_POST["pwd"];
+  $pwd = $_POST["pwd"];
   $pwdrepeat = $_POST["pwdrepeat"];
 
   require_once 'connection.php';
   require_once 'functions.php';
 
-  if(emptyInputRegister() !== false){
+  if (emptyInputRegister() !== false) {
     header("location:../register.php");
     exit();
   }
-
 } else {
 }
-  header("location:../register.php");
+header("location:../register.php");
